5935e42b36493c27bb6dc18318eba20cfa695f60
Kate Chamberlain <kate.chamberlain@softwire.com>
2026-09-09 17:20:20 +0100

    Unit tests - normal items decrease in qual and sell in by 1 each day Unit test - normal items' quality decreases by 2 after sell by date has passed

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -7,6 +7,25 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Test]
+    public void NormalItem_QualityAndSellInDecreaseByDay()
+    {
+        var items = new List<Item> { new Item { Name = "Normal Item", SellIn = 10, Quality = 20 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(19));
+        Assert.That(items[0].SellIn, Is.EqualTo(9));
+    }
+    
+    [Test]
+    public void NormalItem_QualityDegradesTwiceAsFastAfterSellIn()
+    {
+        var items = new List<Item> { new Item { Name = "Normal Item", SellIn = 0, Quality = 20 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(18));
+    }
+    
+    [Test]
     public void QualityNeverNegative()
     {
         var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
