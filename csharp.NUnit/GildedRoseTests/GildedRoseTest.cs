80747c73d94b5a533f8537f087ee2d55ac392c42
Kate Chamberlain <kate.chamberlain@softwire.com>
2026-09-09 17:00:23 +0100

    Unit test: Sulfaras doesn't change quality and SellIn remains at 0

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -14,4 +14,19 @@ public class GildedRoseTest
         app.UpdateQuality();
         Assert.That(items[0].Quality, Is.GreaterThanOrEqualTo(0));
     }
+    
+    [Test]
+    public void Sulfuras_DoesNotChangeSellInOrQuality()
+    {
+        // Arrange
+        var items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 } };
+        var app = new GildedRose(items);
+        
+        // Act
+        app.UpdateQuality();
+        
+        // Assert
+        Assert.That(items[0].Quality, Is.EqualTo(80));
+        Assert.That(items[0].SellIn, Is.EqualTo(0));
+    }
 }
