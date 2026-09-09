100ff9e087c34fa3a0a978dcb1606b015c3cee4a
Kate Chamberlain <kate.chamberlain@softwire.com>
2026-09-09 16:34:11 +0100

    Unit test for checking Quality doesn't go negative

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -7,11 +7,11 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Test]
-    public void Foo()
+    public void QualityNeverNegative()
     {
         var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.That(items[0].Name, Is.EqualTo("fixme"));
+        Assert.That(items[0].Quality, Is.GreaterThanOrEqualTo(0));
     }
 }
